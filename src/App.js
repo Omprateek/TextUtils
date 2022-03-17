0f29cfce90e8d57d14074fb65e484e7b160b675f
Omprateek <76782881+Omprateek@users.noreply.github.com>
2022-03-17 16:08:30 +0530

    Commit 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import { useState } from 'react';
 import './App.css';
 import Alert from './components/Alert';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route,
-//   Link
-// } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
   const[mode,setMode]=useState('light');
   const[alert,setAlert]=useState(null);
@@ -48,19 +48,19 @@ function App() {
   
   return (
     <>
-    {/* <Router> */}
+    <Router>
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
     <Alert alert={alert}/>
     <div className="container my-3">
-    {/* <Routes> */}
-            {/* <Route exact path="/about" element={<About />}> */}
-            {/* </Route> */}
-            {/* <Route exact path="/" element={<TextForm heading="Enter text to analyze"  mode={mode} showAlert={showAlert} />}> */}
+    <Routes>
+            <Route exact path="/about" element={<About />}>
+            </Route>
+            <Route exact path="/" element={<TextForm heading="Enter text to analyze"  mode={mode} showAlert={showAlert} />}>
             <TextForm heading="Enter text to analyze"  mode={mode} showAlert={showAlert} />
-            {/* </Route> */}
-          {/* </Routes> */}
+            </Route>
+          </Routes>
     </div>
-    {/* </Router> */}
+    </Router>
     </>
   );
 }
